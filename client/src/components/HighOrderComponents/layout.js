d04e90904430be60e6a74f02a8ad7958c621383e
Daniel Abib <daniel.abib@gmail.com>
2019-02-26 15:45:11 -0300

    Final Project

---
+++ client/src/components/HighOrderComponents/layout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import Header from '../Header_footer/Header/index';
+import Footer from '../Header_footer/Footer/index';
+
+import { connect } from 'react-redux';
+import { getSiteData } from '../../redux/actions/site_actions';
+
+class Layout extends Component {
+
+
+    componentDidMount(){
+        if(Object.keys(this.props.site).length === 0){
+            this.props.dispatch(getSiteData());
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className="page_container">
+                    {this.props.children}
+                </div>
+                <Footer data={this.props.site}/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        site: state.site
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
